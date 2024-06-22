e7112bd33ff17b0a8c28655069e67fce9517bc4f
EthanHaller <ttk4ey@virginia.edu>
2024-06-21 23:29:12 -0400

    feat: take photo and setup ui kitten

--- App.js
+++ App.js
@@ -1,101 +1,73 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Friends, NewReceipt, Outings, Parties, Profile } from "./tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome6 } from "@expo/vector-icons";
-import { Ionicons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import * as eva from "@eva-design/eva"
+import { ApplicationProvider } from "@ui-kitten/components"
+import { Friends, NewReceipt, Outings, Parties, Profile } from "./views"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { FontAwesome6 } from "@expo/vector-icons"
+import { Ionicons } from "@expo/vector-icons"
+import { AntDesign } from "@expo/vector-icons"
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 const screenOptions = {
-  tabBarShowLabel: true,
-  headerShown: false,
-  tabBarStyle: {
-    backgroundColor: "#fff",
-  },
-};
+	tabBarShowLabel: true,
+	headerShown: false,
+	tabBarStyle: {
+		backgroundColor: "#fff",
+	},
+}
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={screenOptions}>
-        <Tab.Screen
-          name="Outings"
-          component={Outings}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <FontAwesome6
-                  name="receipt"
-                  size={24}
-                  color={focused ? "black" : "gray"}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Parties"
-          component={Parties}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <FontAwesome6
-                  name="people-line"
-                  size={24}
-                  color={focused ? "black" : "gray"}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="New Receipt"
-          component={NewReceipt}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <AntDesign
-                  name="pluscircleo"
-                  size={24}
-                  color={focused ? "black" : "gray"}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Friends"
-          component={Friends}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <Ionicons
-                  name="people"
-                  size={24}
-                  color={focused ? "black" : "gray"}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <Ionicons
-                  name="person"
-                  size={24}
-                  color={focused ? "black" : "gray"}
-                />
-              );
-            },
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<ApplicationProvider {...eva} theme={eva.light}>
+			<NavigationContainer>
+				<Tab.Navigator screenOptions={screenOptions}>
+					<Tab.Screen
+						name="Outings"
+						component={Outings}
+						options={{
+							tabBarIcon: ({ focused }) => {
+								return <FontAwesome6 name="receipt" size={24} color={focused ? "black" : "gray"} />
+							},
+						}}
+					/>
+					<Tab.Screen
+						name="Parties"
+						component={Parties}
+						options={{
+							tabBarIcon: ({ focused }) => {
+								return <FontAwesome6 name="people-line" size={24} color={focused ? "black" : "gray"} />
+							},
+						}}
+					/>
+					<Tab.Screen
+						name="New Receipt"
+						component={NewReceipt}
+						options={{
+							tabBarIcon: ({ focused }) => {
+								return <AntDesign name="pluscircleo" size={24} color={focused ? "black" : "gray"} />
+							},
+						}}
+					/>
+					<Tab.Screen
+						name="Friends"
+						component={Friends}
+						options={{
+							tabBarIcon: ({ focused }) => {
+								return <Ionicons name="people" size={24} color={focused ? "black" : "gray"} />
+							},
+						}}
+					/>
+					<Tab.Screen
+						name="Profile"
+						component={Profile}
+						options={{
+							tabBarIcon: ({ focused }) => {
+								return <Ionicons name="person" size={24} color={focused ? "black" : "gray"} />
+							},
+						}}
+					/>
+				</Tab.Navigator>
+			</NavigationContainer>
+		</ApplicationProvider>
+	)
 }
--- components/TakePhoto.js
+++ components/TakePhoto.js
@@ -1,58 +1,96 @@
-import { useState } from "react";
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  SafeAreaView,
-} from "react-native";
-import { CameraView } from 'expo-camera';
+import React, { useState, useRef } from "react"
+import { StyleSheet, Text, TouchableOpacity, View, Image, SafeAreaView } from "react-native"
+import { CameraView } from "expo-camera"
+import { Button } from "@ui-kitten/components"
 
-const TakePhoto = () => {
-  const [facing, setFacing] = useState("back");
+const TakePhoto = ({ handleConfirmPhoto }) => {
+	const [facing, setFacing] = useState("back")
+	const [photoUri, setPhotoUri] = useState(null)
+	const cameraRef = useRef(null)
 
-  function toggleCameraFacing() {
-    setFacing((current) => (current === "back" ? "front" : "back"));
-  }
+	function toggleCameraFacing() {
+		setFacing((current) => (current === "back" ? "front" : "back"))
+	}
 
-  return (
-    <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </CameraView>
-    </View>
-  );
-};
+	const takePhoto = async () => {
+		if (cameraRef.current) {
+			const photo = await cameraRef.current.takePictureAsync()
+			setPhotoUri(photo.uri)
+		}
+	}
+
+	return (
+		<SafeAreaView style={styles.container}>
+			{photoUri ? (
+				<View style={styles.previewContainer}>
+					<Image source={{ uri: photoUri }} style={styles.previewImage} />
+					<View style={styles.previewActionsContainer}>
+						<Button appearance="outline" onPress={() => setPhotoUri(null)}>
+							Retake
+						</Button>
+						<Button onPress={() => handleConfirmPhoto(photoUri)}>Confirm</Button>
+					</View>
+				</View>
+			) : (
+				<CameraView style={styles.camera} type={facing} ref={cameraRef}>
+					<View style={styles.buttonContainer}>
+						<TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+							<Text style={styles.text}>Flip Camera</Text>
+						</TouchableOpacity>
+						<TouchableOpacity style={styles.button} onPress={takePhoto}>
+							<Text style={styles.text}>Take Photo</Text>
+						</TouchableOpacity>
+					</View>
+				</CameraView>
+			)}
+		</SafeAreaView>
+	)
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "transparent",
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: "flex-end",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
-  },
-});
+	container: {
+		flex: 1,
+		justifyContent: "center",
+	},
+	camera: {
+		flex: 1,
+	},
+	buttonContainer: {
+		flex: 1,
+		flexDirection: "row",
+		backgroundColor: "transparent",
+		margin: 64,
+	},
+	button: {
+		flex: 1,
+		alignSelf: "flex-end",
+		alignItems: "center",
+	},
+	text: {
+		fontSize: 24,
+		fontWeight: "bold",
+		color: "white",
+	},
+	previewContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 16,
+	},
+	previewImage: {
+		width: "80%",
+		height: "80%",
+		marginVertical: 32,
+		resizeMode: "contain",
+		borderRadius: 16,
+	},
+	previewActionsContainer: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	previewActionsButton: {},
+})
 
-export default TakePhoto;
+export default TakePhoto
