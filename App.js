dc099c3e14a914fe382484371bbe3b8c01659676
EthanHaller <ttk4ey@virginia.edu>
2024-06-26 20:56:03 -0400

    create custom theming through uikitten

--- App.js
+++ App.js
@@ -1,102 +1,106 @@
-import { auth } from "./firebase/firebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
-import Landing from "./views/Landing";
-import { useState, useEffect } from "react";
-import * as eva from "@eva-design/eva";
-import { ApplicationProvider, Spinner, Text } from "@ui-kitten/components";
-import { Friends, NewReceipt, Outings, Parties, Profile } from "./views";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { FontAwesome6 } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import { StyleSheet, View } from "react-native";
+import { auth } from "./firebase/firebaseConfig"
+import { onAuthStateChanged } from "firebase/auth"
+import Landing from "./views/Landing"
+import { useState, useEffect } from "react"
+import * as eva from "@eva-design/eva"
+import { ApplicationProvider, Layout, Spinner, Text } from "@ui-kitten/components"
+import { Friends, NewReceipt, Outings, Parties, Profile } from "./views"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { FontAwesome6 } from "@expo/vector-icons"
+import { Ionicons } from "@expo/vector-icons"
+import { View } from "react-native"
+import {useColorScheme} from 'react-native';
+import { default as theme } from "./theme.json"
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 const screenOptions = {
-    tabBarShowLabel: true,
-    headerShown: false,
-    tabBarStyle: {
-        backgroundColor: "#fff",
-    },
-};
+	tabBarShowLabel: true,
+	headerShown: false,
+	tabBarStyle: {
+		backgroundColor: "#fff",
+	},
+}
 
 export default function App() {
-    const [signedIn, setSignedIn] = useState(false);
-    const [loading, setLoading] = useState(true);
+	const [signedIn, setSignedIn] = useState(false)
+	const [loading, setLoading] = useState(true)
+    const colorScheme = useColorScheme()
 
-    useEffect(() => {
-        setLoading(true);
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setSignedIn(true);
-            } else {
-                setSignedIn(false);
-            }
-            setLoading(false);
-        });
-    }, []);
+	useEffect(() => {
+		setLoading(true)
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setSignedIn(true)
+			} else {
+				setSignedIn(false)
+			}
+			setLoading(false)
+		})
+	}, [])
 
-    return (
-        <ApplicationProvider {...eva} theme={eva.light}>
-            {loading ? (
-                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                    <Spinner size="giant" />
-                    <Text style={{ marginVertical: 12 }}>Loading</Text>
-                </View>
-            ) : signedIn ? (
-                <NavigationContainer>
-                    <Tab.Navigator screenOptions={screenOptions}>
-                        <Tab.Screen
-                            name="Outings"
-                            component={Outings}
-                            options={{
-                                tabBarIcon: ({ focused }) => {
-                                    return <FontAwesome6 name="receipt" size={24} color={focused ? "black" : "gray"} />;
-                                },
-                            }}
-                        />
-                        <Tab.Screen
-                            name="Parties"
-                            component={Parties}
-                            options={{
-                                tabBarIcon: ({ focused }) => {
-                                    return <FontAwesome6 name="people-line" size={24} color={focused ? "black" : "gray"} />;
-                                },
-                            }}
-                        />
-                        <Tab.Screen
-                            name="New Receipt"
-                            component={NewReceipt}
-                            options={{
-                                tabBarIcon: ({ focused }) => {
-                                    return <Ionicons name="add-circle-outline" size={24} color={focused ? "black" : "gray"} />;
-                                },
-                            }}
-                        />
-                        <Tab.Screen
-                            name="Friends"
-                            component={Friends}
-                            options={{
-                                tabBarIcon: ({ focused }) => {
-                                    return <Ionicons name="people" size={24} color={focused ? "black" : "gray"} />;
-                                },
-                            }}
-                        />
-                        <Tab.Screen
-                            name="Profile"
-                            component={Profile}
-                            options={{
-                                tabBarIcon: ({ focused }) => {
-                                    return <Ionicons name="person" size={24} color={focused ? "black" : "gray"} />;
-                                },
-                            }}
-                        />
-                    </Tab.Navigator>
-                </NavigationContainer>
-            ) : (
-                <Landing />
-            )}
-        </ApplicationProvider>
-    );
+	return (
+		<ApplicationProvider {...eva} theme={colorScheme=== "dark" ? { ...eva.dark, ...theme } : { ...eva.light, ...theme }}>
+			<Layout style={{ flex: 1 }}>
+				{loading ? (
+					<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+						<Spinner size="giant" />
+						<Text style={{ marginVertical: 12 }}>Loading</Text>
+					</View>
+				) : signedIn ? (
+					<NavigationContainer>
+						<Tab.Navigator screenOptions={screenOptions}>
+							<Tab.Screen
+								name="Outings"
+								component={Outings}
+								options={{
+									tabBarIcon: ({ focused }) => {
+										return <FontAwesome6 name="receipt" size={24} color={focused ? "black" : "gray"} />
+									},
+								}}
+							/>
+							<Tab.Screen
+								name="Parties"
+								component={Parties}
+								options={{
+									tabBarIcon: ({ focused }) => {
+										return <FontAwesome6 name="people-line" size={24} color={focused ? "black" : "gray"} />
+									},
+								}}
+							/>
+							<Tab.Screen
+								name="New Receipt"
+								component={NewReceipt}
+								options={{
+									tabBarIcon: ({ focused }) => {
+										return <Ionicons name="add-circle-outline" size={24} color={focused ? "black" : "gray"} />
+									},
+								}}
+							/>
+							<Tab.Screen
+								name="Friends"
+								component={Friends}
+								options={{
+									tabBarIcon: ({ focused }) => {
+										return <Ionicons name="people" size={24} color={focused ? "black" : "gray"} />
+									},
+								}}
+							/>
+							<Tab.Screen
+								name="Profile"
+								component={Profile}
+								options={{
+									tabBarIcon: ({ focused }) => {
+										return <Ionicons name="person" size={24} color={focused ? "black" : "gray"} />
+									},
+								}}
+							/>
+						</Tab.Navigator>
+					</NavigationContainer>
+				) : (
+					<Landing />
+				)}
+			</Layout>
+		</ApplicationProvider>
+	)
 }
